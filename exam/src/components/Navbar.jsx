8b582dbd44640035b2d4625fa45c5b98f282c0f9
Mijael Aliaga <82177948+Mijael-cod@users.noreply.github.com>
2023-04-18 11:37:09 -0500

    Update Navbar.jsx

--- exam/src/components/Navbar.jsx
+++ exam/src/components/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <span className="logo">Welcome!!</span>
+      <span className="logo">Bienvenido !!</span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>logout</button>
+        <button onClick={()=>signOut(auth)}>cerrar sesión</button>
       </div>
     </div>
   )
